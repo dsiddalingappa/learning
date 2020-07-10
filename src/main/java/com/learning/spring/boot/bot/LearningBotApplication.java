22fa1a02fd171b9814f62e71e4b663ffb99d95b0
dsiddalingappa <dsiddalingappa@extremenetworks.com>
2020-07-10 11:40:26 +0530

    Added a REST Endpoint

--- src/main/java/com/learning/spring/boot/bot/LearningBotApplication.java
+++ src/main/java/com/learning/spring/boot/bot/LearningBotApplication.java
@@ -2,9 +2,17 @@ package com.learning.spring.boot.bot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LearningBotApplication {
+
+	@RequestMapping("/")
+	public String learningHome() {
+		return "Happy bot learning";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningBotApplication.class, args);
